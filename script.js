49408fda048332b104fac9c9ca4cccb23e6b72e4
omar5hahid <145630796+omar5hahid@users.noreply.github.com>
2023-11-10 05:48:10 -0500

    basicsitedone

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-ime
+
+document.addEventListener('DOMContentLoaded', function () {
+    const targetDate = new Date('December 25, 2023').getTime();
+  
+    function updateCountdown() {
+      const currentDate = new Date().getTime();
+      const timeDifference = targetDate - currentDate;
+  
+      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+  
+      document.getElementById('days').innerText = days;
+      document.getElementById('hours').innerText = hours;
+      document.getElementById('minutes').innerText = minutes;
+      document.getElementById('seconds').innerText = seconds;
+    }
+  
+    updateCountdown();
+  
+    setInterval(updateCountdown, 1000);
+  });
+  
+
